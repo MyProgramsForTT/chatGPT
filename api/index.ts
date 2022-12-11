a8b469d0ebe2ad06f39b34fa3aa28a136f1e20d7
MyProgramsForTT <70446332+MyProgramsForTT@users.noreply.github.com>
2022-12-11 05:59:14 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ api/index.ts
@@ -0,0 +1,49 @@
+import {VercelRequest, VercelResponse} from '@vercel/node'
+import {ChatGPTAPI} from "chatgpt";
+
+// require('dotenv').config();
+
+async function sendMessage(message: string) {
+    let token = process.env.SESSION_TOKEN;
+    if (`${token}`.trim().length <= 1) {
+        throw new Error('Env "SESSION_TOKEN" Not Found');
+    }
+    let api = new ChatGPTAPI({sessionToken: token!});
+
+    // ensure the API is properly authenticated
+    await api.ensureAuth()
+
+    // send a message and wait for the response
+    return await api.sendMessage(message);
+}
+
+async function sendMessageThrow(ask: string) {
+    let response: any = {
+        ask,
+        message: "ok",
+        status: 200,
+        success: true,
+    };
+    try {
+        response.data = await sendMessage(ask)
+    } catch (err: any) {
+        console.warn(err)
+        response = {ask, status: 400, success: false, message: `${err?.message}`}
+    }
+    return response;
+}
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+    const ask = req.query.ask as string
+    let response: any = await sendMessageThrow(ask);
+    res.status(response.status);
+    res.json(response)
+}
+
+// for dev
+if (process.env.NODE_ENV === 'development') {
+    (async () => {
+        const res = await sendMessageThrow(`什么是Java`)
+        console.log(res)
+    })();
+}
